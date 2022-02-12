b5607729bdb0a047d05b3b388ebb2412457de7b1
iamzsombi <zsombinagy.14@gmail.com>
2022-02-12 19:15:39 +0100

    home page next step

--- comp/Navbar.tsx
+++ comp/Navbar.tsx
@@ -2,21 +2,25 @@ import styles from "../styles/Navbar.module.sass";
 import { HomeIcon, BellIcon, UserIcon, MoonIcon, GlobeIcon } from "@iconicicons/react"
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [dark, setDark] = useState("");
+
+    
     return (
-        <div className={styles.navbar}>
+        <div className={styles.navbar + " " + dark}>
             
             <div className={styles.upper_icons}>
                 <div className={styles.icon_div}><Link href="/"><HomeIcon className={styles.icon} /></Link></div>
-                <div className={styles.icon_div}><Link href="/"><GlobeIcon className={styles.icon} /></Link></div>
-                <div className={styles.icon_div}><Link href="/"><UserIcon className={styles.icon} /></Link></div>
+                <div className={styles.icon_div }><Link href="/"><GlobeIcon className={styles.icon} /></Link></div>
                 <div className={styles.icon_div}><Link href="/"><BellIcon className={styles.icon} /></Link></div>
+                <div className={styles.icon_div }><Link href="/"><UserIcon className={styles.icon} /></Link></div>
 
             </div>
             <div className={styles.below_icon}>
                 <div className={styles.icon_div}>
-                    <Link href="/"><MoonIcon className={styles.icon } /></Link>
+                    <button><MoonIcon className={styles.icon } /></button>
             
                 </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.sass';
 import Navbar from '../comp/Navbar';
 import { getFeed } from "public-square-api";
 import Link from 'next/link';
-import { PhotoIcon, EmojiHappyIcon, SendIcon } from "@iconicicons/react";
+import { PhotoIcon, EmojiHappyIcon, SendIcon, HeartIcon, ShareIcon } from "@iconicicons/react";
 
 const Home: NextPage = () => {
 
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
   return (
     <>
       <div className={styles.home_page}>
-        <div className={styles.happening_container}>
+        <div className={styles.home_page_containers + " " + styles.top_container}>
           <Link href="/">
             <div className={styles.profile_div}>
               <Image
@@ -40,10 +40,43 @@ const Home: NextPage = () => {
               <SendIcon className={styles.weeve_icon} />
             </button>  
             
-          </div>
-            
+            </div>
+          
           </div>
         </div>
+        <div className={styles.home_page_containers}>
+          <div className={styles.upper_post}>
+            <div className={styles.profile_div}>
+              <Image
+                  width="100%"
+                  height="100%"
+                  src="/profile.jpg"
+                  alt="profile-picutre"
+              />
+            </div>
+            <div className={styles.user_name}>Axel</div>
+            <div className={styles.profile_name}>@bidetaggle</div>
+            <div className={styles.time}>3 hours ago</div>
+            <div className={styles.link_post}><a>sf3ZF...eBCK4</a></div>
+          </div>
+          <div className={styles.post_content}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+          <div className={styles.below_post}>
+            <div className={styles.comment_section}>
+              10 Comments
+            </div>
+            <div className={styles.like_section}>
+              <HeartIcon className={styles.post_icons}/>
+              28 likes
+            </div>
+            <div className={styles.share_section}>
+              <ShareIcon  className={styles.post_icons}/>
+              share
+            </div>
+
+          </div>
+          
+        </div>
+
       </div>
 
 
