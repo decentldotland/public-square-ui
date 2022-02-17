f5680a103447cb381d9777ab9fa09d11c636806a
iamzsombi <zsombinagy.14@gmail.com>
2022-02-17 23:23:25 +0100

    home page content flex box

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,12 +24,7 @@ const Home: NextPage = () => {
           <div className={styles.upper_items}>
             <Link href="/">
               <div className={styles.profile_div}>
-                <Image
-                  width="100%"
-                  height="100%"
-                  src="/profile.jpg"
-                  alt="profile-picutre"
-                />
+              <img src="/profile.png" alt="profile-picutre" />
               </div>
             </Link>
 
@@ -53,6 +48,44 @@ const Home: NextPage = () => {
           <div className={styles.upper_post}>
             <div className={styles.left_section}>
               <div className={styles.profile_div}> 
+                <img src="/profile.png" alt="profile-picutre" />
+              </div>
+              <div className={styles.data_user}>
+                <div className={styles.user_name}>Axel</div>
+                <div className={styles.profile_name}>@bidetaggle</div>
+              </div>
+            </div>
+            <div className={styles.right_section}>
+              <div className={styles.time}>3 hours ago</div>
+              <div className={styles.link_post}>
+                <a>sf3ZF...eBCK4</a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.post_content}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </div>
+          <div className={styles.below_post}>
+            <div className={styles.comment_section}> <MessageIcon className={styles.post_icons}/> 10 Comments</div>
+            <div className={styles.like_section}>
+              <HeartIcon className={styles.post_icons} />
+              28 likes
+            </div>
+            <div className={styles.share_section}>
+              <ShareIcon className={styles.post_icons} />
+              share
+            </div>
+          </div>
+        </div>
+        <div className={styles.home_page_containers}>
+          <div className={styles.upper_post}>
+            <div className={styles.left_section}>
+              <div className={styles.profile_div}>
               <img src="/profile.png" alt="profile-picutre" />
               </div>
               <div className={styles.data_user}>
@@ -91,55 +124,7 @@ const Home: NextPage = () => {
           <div className={styles.upper_post}>
             <div className={styles.left_section}>
               <div className={styles.profile_div}>
-                <Image
-                  width="100%"
-                  height="100%"
-                  src="/profile.jpg"
-                  alt="profile-picutre"
-                />
-              </div>
-              <div className={styles.data_user}>
-                <div className={styles.user_name}>Axel</div>
-                <div className={styles.profile_name}>@bidetaggle</div>
-              </div>
-            </div>
-            <div className={styles.right_section}>
-              <div className={styles.time}>3 hours ago</div>
-              <div className={styles.link_post}>
-                <a>sf3ZF...eBCK4</a>
-              </div>
-            </div>
-          </div>
-          <div className={styles.post_content}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-          </div>
-          <div className={styles.below_post}>
-            <div className={styles.comment_section}> <MessageIcon className={styles.post_icons}/> 10 Comments</div>
-            <div className={styles.like_section}>
-              <HeartIcon className={styles.post_icons} />
-              28 likes
-            </div>
-            <div className={styles.share_section}>
-              <ShareIcon className={styles.post_icons} />
-              share
-            </div>
-          </div>
-        </div>
-        <div className={styles.home_page_containers}>
-          <div className={styles.upper_post}>
-            <div className={styles.left_section}>
-              <div className={styles.profile_div}>
-                <Image
-                  width="100%"
-                  height="100%"
-                  src="/profile.jpg"
-                  alt="profile-picutre"
-                />
+              <img src="/profile.png" alt="profile-picutre" />
               </div>
               <div className={styles.data_user}>
                 <div className={styles.user_name}>Axel</div>
