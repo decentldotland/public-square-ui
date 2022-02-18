4fe2894abdf63bbfb0de078940b7e1c5d5548242
iamzsombi <zsombinagy.14@gmail.com>
2022-02-18 11:28:04 +0100

    right-side flex-box started

--- comp/Right_Side.tsx
+++ comp/Right_Side.tsx
@@ -7,30 +7,20 @@ const Right_Side = () => {
     <>
       <div className={styles.right_side}>
         <div className={styles.searcher}>
-          <form>
-            <input
-              type="text"
-              placeholder="@username / wallet id / community"
-            />
-          </form>
+          <input type="text" placeholder="@username / wallet id / community" />
           <div className={styles.icon_div}>
             <SearchIcon className={styles.icon} />
           </div>
         </div>
         <div className={styles.suggestion_section}>
-          <div className={styles.title}>Suggestion</div>
-          <div className={styles.button}>
-            <button>See All</button>
+          <div className={styles.title_section}>
+            <div className={styles.title}>Suggestion</div>
+            <button className={styles.title_button}>See All</button>
           </div>
           <div className={styles.suggestions_div}>
             <div className={styles.suggestion_div}>
               <div className={styles.profile_div}>
-                <Image
-                  width="100%"
-                  height="100%"
-                  src="/profile.jpg"
-                  alt="profile-picutre"
-                />
+              <img src="/profile.png" alt="profile-picutre" />
               </div>
               <div className={styles.user_name}>Marton Lederer</div>
               <div className={styles.profile_name}>@martonlederer</div>
@@ -42,12 +32,7 @@ const Right_Side = () => {
           <div className={styles.suggestions_div}>
             <div className={styles.suggestion_div}>
               <div className={styles.profile_div}>
-                <Image
-                  width="100%"
-                  height="100%"
-                  src="/profile.jpg"
-                  alt="profile-picutre"
-                />
+              <img src="/profile.png" alt="profile-picutre" />
               </div>
               <div className={styles.user_name}>Zsombor Nagy</div>
               <div className={styles.profile_name}>@zsombinagy</div>
