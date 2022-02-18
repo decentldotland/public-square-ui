1ed81b21039bc8ea8ac7cbdc0316e99394d21f0f
iamzsombi <zsombinagy.14@gmail.com>
2022-02-18 19:02:30 +0100

    notifications site started flexbox

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,5 +1,10 @@
 import styles from "../styles/Notifications.module.sass";
-import { HeartIcon, UserPlusIcon, ShareIcon } from "@iconicicons/react";
+import {
+  HeartIcon,
+  UserPlusIcon,
+  ShareIcon,
+  MessageIcon,
+} from "@iconicicons/react";
 import Image from "next/image";
 
 const Notifications = () => {
@@ -10,24 +15,25 @@ const Notifications = () => {
           <h1>Notifications</h1>
           <div className={styles.notification + " " + styles.like_notification}>
             <h2>
-              <HeartIcon className={styles.post_icons + " " + styles.heart} />{" "}
+              <HeartIcon className={styles.post_icons + " " + styles.heart} />
               <span> Marton Lederer</span> likes your weeve
             </h2>
             <div className={styles.notification_containers}>
               <div className={styles.upper_post}>
-                <div className={styles.profile_div}>
-                  <Image
-                    width="100%"
-                    height="100%"
-                    src="/profile.jpg"
-                    alt="profile-picutre"
-                  />
+                <div className={styles.left_section}>
+                  <div className={styles.profile_div}>
+                    <img src="/profile.png" alt="profile-picutre" />
+                  </div>
+                  <div className={styles.data_user}>
+                    <div className={styles.user_name}>Axel</div>
+                    <div className={styles.profile_name}>@bidetaggle</div>
+                  </div>
                 </div>
-                <div className={styles.user_name}>Axel</div>
-                <div className={styles.profile_name}>@bidetaggle</div>
-                <div className={styles.time}>3 hours ago</div>
-                <div className={styles.link_post}>
-                  <a>sf3ZF...eBCK4</a>
+                <div className={styles.right_section}>
+                  <div className={styles.time}>3 hours ago</div>
+                  <div className={styles.link_post}>
+                    <a>sf3ZF...eBCK4</a>
+                  </div>
                 </div>
               </div>
               <div className={styles.post_content}>
@@ -39,7 +45,10 @@ const Notifications = () => {
                 electronic typesetting, remaining essentially unchanged.
               </div>
               <div className={styles.below_post}>
-                <div className={styles.comment_section}>10 Comments</div>
+                <div className={styles.comment_section}>
+                  {" "}
+                  <MessageIcon className={styles.post_icons} /> 10 Comments
+                </div>
                 <div className={styles.like_section}>
                   <HeartIcon className={styles.post_icons} />
                   28 likes
@@ -62,25 +71,22 @@ const Notifications = () => {
             </h2>
             <div className={styles.notification_containers}>
               <div className={styles.upper_post}>
-                <div className={styles.profile_div}>
-                  <Image
-                    width="100%"
-                    height="100%"
-                    src="/profile.jpg"
-                    alt="profile-picutre"
-                  />
+                <div className={styles.data_users}>
+                  <div className={styles.profile_div}>
+                    <img src="/profile.png" alt="profile-picutre" />
+                  </div>
+                  <div className={styles.user_name}>Axel</div>
+                  <div className={styles.profile_name}>@bidetaggle</div>
                 </div>
-                <div className={styles.user_name}>Axel</div>
-                <div className={styles.profile_name}>@bidetaggle</div>
+                <div className={styles.follow_button}>
+                  <button>follow</button>
+                </div>
               </div>
               <div className={styles.post_content}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book.
-              </div>
-              <div className={styles.follow_button}>
-                  <button>follow</button>
               </div>
             </div>
           </div>
@@ -93,19 +99,20 @@ const Notifications = () => {
             </h2>
             <div className={styles.notification_containers}>
               <div className={styles.upper_post}>
-                <div className={styles.profile_div}>
-                  <Image
-                    width="100%"
-                    height="100%"
-                    src="/profile.jpg"
-                    alt="profile-picutre"
-                  />
+                <div className={styles.left_section}>
+                  <div className={styles.profile_div}>
+                    <img src="/profile.png" alt="profile-picutre" />
+                  </div>
+                  <div className={styles.data_user}>
+                    <div className={styles.user_name}>Axel</div>
+                    <div className={styles.profile_name}>@bidetaggle</div>
+                  </div>
                 </div>
-                <div className={styles.user_name}>Axel</div>
-                <div className={styles.profile_name}>@bidetaggle</div>
-                <div className={styles.time}>3 hours ago</div>
-                <div className={styles.link_post}>
-                  <a>sf3ZF...eBCK4</a>
+                <div className={styles.right_section}>
+                  <div className={styles.time}>3 hours ago</div>
+                  <div className={styles.link_post}>
+                    <a>sf3ZF...eBCK4</a>
+                  </div>
                 </div>
               </div>
               <div className={styles.post_content}>
@@ -117,7 +124,9 @@ const Notifications = () => {
                 electronic typesetting, remaining essentially unchanged.
               </div>
               <div className={styles.below_post}>
-                <div className={styles.comment_section}>10 Comments</div>
+                <div className={styles.comment_section}>
+                  <MessageIcon className={styles.post_icons} /> 10 Comments
+                </div>
                 <div className={styles.like_section}>
                   <HeartIcon className={styles.post_icons} />
                   28 likes
