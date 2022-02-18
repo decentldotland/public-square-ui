38be9399cc8b14a4c7cc4eb64d91c8f8cf5d3563
iamzsombi <zsombinagy.14@gmail.com>
2022-02-18 17:32:00 +0100

    right side flex-box ready

--- comp/Right_Side.tsx
+++ comp/Right_Side.tsx
@@ -6,46 +6,60 @@ const Right_Side = () => {
   return (
     <>
       <div className={styles.right_side}>
-        <div className={styles.searcher}>
-          <input type="text" placeholder="@username / wallet id / community" />
-          <div className={styles.icon_div}>
-            <SearchIcon className={styles.icon} />
-          </div>
-        </div>
-        <div className={styles.suggestion_section}>
-          <div className={styles.title_section}>
-            <div className={styles.title}>Suggestion</div>
-            <button className={styles.title_button}>See All</button>
-          </div>
-          <div className={styles.suggestions_div}>
-            <div className={styles.suggestion_div}>
-              <div className={styles.profile_div}>
-              <img src="/profile.png" alt="profile-picutre" />
-              </div>
-              <div className={styles.user_name}>Marton Lederer</div>
-              <div className={styles.profile_name}>@martonlederer</div>
-              <div className={styles.button_div}>
-                <button>Follow</button>
-              </div>
+        <div className={styles.upper_section}>
+          <div className={styles.searcher}>
+            <input
+              type="text"
+              placeholder="@username / wallet id / community"
+            />
+            <div className={styles.icon_div}>
+              <SearchIcon className={styles.icon} />
             </div>
           </div>
-          <div className={styles.suggestions_div}>
-            <div className={styles.suggestion_div}>
-              <div className={styles.profile_div}>
-              <img src="/profile.png" alt="profile-picutre" />
+          <div className={styles.suggestion_section}>
+            <div className={styles.title_section}>
+              <div className={styles.title}>Suggestion</div>
+              <button className={styles.title_button}>See All</button>
+            </div>
+            <div className={styles.suggestions_div}>
+              <div className={styles.suggestion_div}>
+                <div className={styles.profile_data}>
+                  <div className={styles.profile_div}>
+                    <img src="/profile.png" alt="profile-picutre" />
+                  </div>
+                  <div className={styles.data_users}>
+                    <div className={styles.user_name}>Marton Lederer</div>
+                    <div className={styles.profile_name}>@martonlederer</div>
+                  </div>
+                </div>
+                <div className={styles.button_div}>
+                  <button>Follow</button>
+                </div>
               </div>
-              <div className={styles.user_name}>Zsombor Nagy</div>
-              <div className={styles.profile_name}>@zsombinagy</div>
-              <div className={styles.button_div}>
-                <button>Follow</button>
+
+              <div className={styles.suggestion_div}>
+                <div className={styles.profile_data}>
+                  <div className={styles.profile_div}>
+                    <img src="/profile.png" alt="profile-picutre" />
+                  </div>
+                  <div className={styles.data_users}>
+                    <div className={styles.user_name}>Zsombor Nagy</div>
+                    <div className={styles.profile_name}>@zsombinagy</div>
+                  </div>
+                </div>
+                <div className={styles.button_div}>
+                  <button>Follow</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className={styles.footer}>
-          <a>Terms of Service</a>
-          <a>Privacy Policy</a>
-          <p> &copy;2021,Argora</p>
+          <div className={styles.left_footer}>
+            <a>Terms of Service</a>
+            <a>Privacy Policy</a>
+          </div>
+          <div className={styles.copyright}> &copy;2021,Argora</div>
         </div>
       </div>
     </>
