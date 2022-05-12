7b7444d090379c12298870e7a732e8ea0242d0f1
iamzsombi <zsombinagy.14@gmail.com>
2022-05-12 19:46:00 +0200

    feat: favicon set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app";
 
 import Right_Side from "../comp/Right_side/Right_Side";
 import Navbar from "../comp/Navbar/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Right_Side />
